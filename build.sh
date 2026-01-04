d52c37b3a5c776aee5bd77e73200145e8e5789c3
panzertype <rellowy@gmail.com>
2026-01-04 13:52:13 +0200

    Moved to CMake

--- build.sh
+++
@@ -1,8 +0,0 @@
-COMPILER=clang
-RAYLIB_INCLUDE=/opt/homebrew/Cellar/raylib/5.5/include
-RAYLIB_LIB=/opt/homebrew/Cellar/raylib/5.5/lib
-
-$COMPILER -g --std=c99 ./src/main.c ./src/chip8.c \
-    -o chip8 \
-    -Wall -Werror \
-    -I$RAYLIB_INCLUDE -L$RAYLIB_LIB -lraylib
